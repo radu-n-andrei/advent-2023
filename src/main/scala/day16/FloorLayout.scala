84bd2a71f6c949432355273428d298338456e296
Radu Andrei <andrei.radu.niculian@gmail.com>
2023-12-16 15:07:48 +0200

    day 16 fix

--- src/main/scala/day16/FloorLayout.scala
+++ src/main/scala/day16/FloorLayout.scala
@@ -4,81 +4,91 @@ import scala.annotation.tailrec
 
 final case class FloorLayout(tiles: List[List[Tile]]) {
 
-    val width = tiles.headOption.fold(0)(_.length)
-    val height = tiles.length
+  val width = tiles.headOption.fold(0)(_.length)
+  val height = tiles.length
 
-    val tilesAsMap = 
-            tiles.zipWithIndex.flatMap {
-                case(l, y) => l.zipWithIndex.map {
-                    case (t, x) => (Coordinate(x, y) -> t)
-                }
-            }.toMap
+  val tilesAsMap =
+    tiles.zipWithIndex.flatMap { case (l, y) =>
+      l.zipWithIndex.map { case (t, x) =>
+        (Coordinate(x, y) -> t)
+      }
+    }.toMap
 
-    def print: Unit = 
-        tiles.foreach(t => println(t.map(_.symbol).mkString))
-    
-    def energizeFrom(c: Coordinate, towards: Direction): Int = {
+  def print: Unit =
+    tiles.foreach(t => println(t.map(_.symbol).mkString))
 
-        //tine map de la (coordinate, direction) -> energized
-        @tailrec
-        def enterTile(coordinates: List[(Coordinate, Direction)], acc: List[(Coordinate, Direction)]): Int = {
-            coordinates match {
-                case Nil => acc.map(_._1).distinct.length
-                case c :: coords if acc.contains(c) => enterTile(coords, acc)
-                case (coord, direction) :: coords => 
-                    val nextDirs = tilesAsMap(coord).energize(direction)
-                    val nextTileCoords = nextDirs.map(d => (d.next(coord), d.opposite)).filter {
-                        case (newCoord, _) => newCoord.x >= 0 && newCoord.x < width && newCoord.y >=0 && newCoord.y < height
-                    }
-                    enterTile(nextTileCoords ++ coords, (coord, direction) +: acc)
-            }
-            
+  @tailrec
+  def enterTileWithShortcuts(
+      coordinates: List[(Coordinate, Direction)],
+      acc: List[(Coordinate, Direction)],
+      shortcuts: Map[(Coordinate, Direction), List[(Coordinate, Direction)]]
+  ): (
+      Int,
+      Map[(Coordinate, Direction), List[(Coordinate, Direction)]]
+  ) =
+    coordinates match {
+      case Nil => (acc.map(_._1).distinct.length, shortcuts)
+      case c :: coords if acc.contains(c) =>
+        enterTileWithShortcuts(coords, acc, shortcuts)
+      case (coord, direction) :: coords =>
+        val nextDirs = tilesAsMap(coord).energize(direction)
+        val nextTileCoords =
+          nextDirs.map(d => (d.next(coord), d.opposite)).filter {
+            case (newCoord, _) =>
+              newCoord.x >= 0 && newCoord.x < width && newCoord.y >= 0 && newCoord.y < height
+          }
+        if (nextTileCoords.isEmpty) {
+          enterTileWithShortcuts(
+            coords,
+            (coord, direction) +: acc,
+            shortcuts + ((coord, direction) -> List((coord, direction)))
+          )
+        } else if (nextTileCoords.forall(shortcuts.isDefinedAt)) {
+          val short = nextTileCoords.flatMap(shortcuts(_))
+          val currentEnerg = 1 + short.map(_._1).distinct.length
+          enterTileWithShortcuts(
+            coords,
+            ((coord, direction) +: short) ++ acc,
+            shortcuts + ((coord, direction) -> ((coord, direction) +: short))
+          )
+        } else {
+          val short = nextTileCoords.flatMap(shortcuts.get).flatten.distinct
+          val passed = (coord, direction) +: short
+          val next = nextTileCoords.filterNot(shortcuts.isDefinedAt)
+          enterTileWithShortcuts(
+            next ++ coords,
+            passed ++ acc,
+            shortcuts
+          )
         }
-
-        
-        enterTile(List((c, towards)), List.empty)
-    }  
-
-    
-         @tailrec
-        def enterTileWithExits(coordinates: List[(Coordinate, Direction)], acc: List[(Coordinate, Direction)], exits: List[Coordinate]): (Int, List[Coordinate]) = {
-            coordinates match {
-                case Nil => (acc.map(_._1).distinct.length, exits)
-                case c :: coords if acc.contains(c) => enterTileWithExits(coords, acc, exits)
-                case (coord, direction) :: coords => 
-                    val nextDirs = tilesAsMap(coord).energize(direction)
-                    val nextTileCoords = nextDirs.map(d => (d.next(coord), d.opposite)).filter {
-                        case (newCoord, _) => newCoord.x >= 0 && newCoord.x < width && newCoord.y >=0 && newCoord.y < height
-                    }
-                    if(nextTileCoords.isEmpty) {
-                        enterTileWithExits(coords, (coord, direction) +: acc, coord +: exits)
-                    } else 
-                        enterTileWithExits(nextTileCoords ++ coords, (coord, direction) +: acc, exits)
-            }
-            
-        }
-
-
-    def maxEnergy: Int = {
-        val northBound = (0 until width).map(i => (Coordinate(i, 0), North))
-        val southBound = (0 until width).map(i => (Coordinate(i, height - 1), South))
-        val eastBound = (0 until height).map(i => (Coordinate(width - 1, i), East))
-        val westBound = (0 until height).map(i => (Coordinate(0, i), West))
-
-        // map!
-        val allBounds = northBound ++ southBound ++ eastBound ++ westBound
-        allBounds.foldLeft((List.empty[Coordinate], 0)) {
-            case ((exits, acc), start) => 
-                println(s"Exploring $start")
-                if(exits.contains(start._1)) {
-                println(s"SKIPPING $start")
-                (exits, acc) 
-            }
-            else {
-                val (energ, exitsReached) = enterTileWithExits(List(start), List.empty, List.empty)
-                (exits ++ exitsReached, Math.max(acc, energ))
-            } 
-        }._2
     }
+
+  def maxEnergy: Int = {
+    val northBound = (0 until width).map(i => (Coordinate(i, 0), North))
+    val southBound =
+      (0 until width).map(i => (Coordinate(i, height - 1), South))
+    val eastBound = (0 until height).map(i => (Coordinate(width - 1, i), East))
+    val westBound = (0 until height).map(i => (Coordinate(0, i), West))
+
+    // map!
+    val allBounds = northBound ++ southBound ++ eastBound ++ westBound
+    allBounds
+      .foldLeft(
+        (
+          0,
+          Map.empty[(Coordinate, Direction), List[(Coordinate, Direction)]]
+        )
+      ) { case ((acc, shortcuts), start) =>
+        //println(s"Processing $start")
+        val (energ, shorts) =
+          enterTileWithShortcuts(
+            List(start),
+            List.empty,
+            shortcuts
+          )
+        (Math.max(acc, energ), shorts)
+      }
+      ._1
+  }
 
 }
--- src/main/scala/day16/FloorWillBeLava.scala
+++ src/main/scala/day16/FloorWillBeLava.scala
@@ -8,8 +8,12 @@ object FloorWillBeLava {
         val file = Source.fromResource("day16.input")
         val input = file.getLines().toList
         val floorLayout = readInput(input, List.empty)
-        println(s"SOL1: ${floorLayout.energizeFrom(Coordinate(0, 0), West)}")
+        //println(s"SOL1: ${floorLayout.energizeFrom(Coordinate(0, 0), West)}")
+        println(s"SOL1: ${floorLayout.enterTileWithShortcuts(List((Coordinate(0, 0), West)), List.empty, Map.empty)._1}")
+        val cTime = System.currentTimeMillis()
         println(s"SOL2: ${floorLayout.maxEnergy}")
+        val eTime =System.currentTimeMillis()
+        println(s"Total: ${eTime - cTime}")
         file.close
     }
 
